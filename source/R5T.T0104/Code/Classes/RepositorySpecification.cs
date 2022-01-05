96ed6b05531f567647fcc5c8777019854bd63a51
David Coats <david@getsafetycone.com>
2022-01-05 12:41:58 -0800

    Initial implementation.

---
+++ source/R5T.T0104/Code/Classes/RepositorySpecification.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0104
+{
+    public class RepositorySpecification
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsPrivate { get; set; }
+    }
+}
